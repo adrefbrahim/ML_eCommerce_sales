14484961e10a2e2a839221841d58ea60fc15f9c8
ADREF Brahim <adrefbrahim@gmail.com>
2019-11-28 00:56:43 +0100

    computing of average basket

--- orders_products_analysis.py
+++ orders_products_analysis.py
@@ -5,3 +5,42 @@ Created on Wed Nov 27 22:47:41 2019
 @author: Admin
 """
 
+import pandas as pd
+
+
+
+
+
+dataOrders = pd.read_csv("C:\\Users\\Admin\\Desktop\\Home\\Studies\\PFE\\ML_eCommerce_sales\\data\\orders.csv")
+data_columns = dataOrders.columns.tolist()
+dataItems = pd.read_csv("C:\\Users\\Admin\\Desktop\\Home\\Studies\\PFE\\ML_eCommerce_sales\\data\\items.csv")
+data_columns = dataItems.columns.tolist()
+dataProducts = pd.read_csv("C:\\Users\\Admin\\Desktop\\Home\\Studies\\PFE\\ML_eCommerce_sales\\data\\products.csv")
+data_columns = dataProducts.columns.tolist()
+dataCustomer = pd.read_csv("C:\\Users\\Admin\\Desktop\\Home\\Studies\\PFE\\ML_eCommerce_sales\\data\\customer.csv")
+data_columns = dataCustomer.columns.tolist()
+
+
+# comprendre les données 
+
+all_orders_customer = dataOrders[dataOrders["customer_id"] == "9ef432eb6251297304e76186b10a928d"]
+
+id = dataItems[dataItems["order_id"] == "00010242fe8c5a6d1ba2dd792cb16214"]["order_item_id"].sum()
+
+
+orders = dataOrders["order_id"]
+
+nb_item = pd.Series()
+
+max_items = 0
+for order in orders:
+    nb_item = dataItems[dataItems["order_id"] == order]["order_item_id"].sum()
+    if (nb_item > max_items):
+        max_items = nb_item
+        
+sum_items = dataItems["order_item_id"].sum()
+average_basket = sum_items / len(dataOrders)
+
+
+        
+    
